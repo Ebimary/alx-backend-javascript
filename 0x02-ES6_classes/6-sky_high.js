8f15b4de1b0066e42a2b924503818cdcc9b6b0f9
Ebimary <igwemary220@gmail.com>
2023-09-28 11:06:18 -0700

    classes js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,11 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return (this._floors);
+  }
